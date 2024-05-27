1c0956c470e38eaecfb3d2e749f59369d0b8b324
Martin Cigarsky <martin.cigarsky@gmail.com>
2024-05-27 10:08:44 +0200

    added error email

--- src/Slack/SlackWs.php
+++ src/Slack/SlackWs.php
@@ -60,6 +60,7 @@ class SlackWs
         $email->from('rosetta@sentia.sk')
         ->addTo('stefan.soltys@sentia.sk')
         ->addTo('branislav.bobak@sentia.sk')
+        ->addTo('martin.cigarsky@sentia.sk')
         ->addTo('diana.kokolusova@sentia.sk')
         ->subject((strtolower($this->params->get('app_env')) === 'prod' ? '[PROD]' : ' [TEST]') . ' ' . $subject)
         ->text($message);
